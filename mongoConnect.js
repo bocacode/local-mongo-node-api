d830fd9cca9b9c97e82841be7227eedc310faafe
Damian Montero <photoca@hotmail.com>
2022-08-06 09:04:35 -0400

    Adding documentation and changing it to hide the credentials.js file

--- mongoConnect.js
+++ mongoConnect.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb'
+ 
+import { uri } from "./credentials.js"
 
-const uri = 'mongodb://localhost:27017'
+// const uri = 'mongodb://localhost:27017'
 
 const client = new MongoClient(uri) // creating client mongo instance
 const db = client.db('bocacode-test') // connecting to db = bocacod-test
